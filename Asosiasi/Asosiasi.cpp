23b66aab5002ad0cdc318bd7194e4b7102aa1bf3
wawanpi <muhaguskurniawan@gmail.com>
2023-05-22 19:00:59 +0700

    membuat distructor passien

--- Asosiasi/Asosiasi.cpp
+++ Asosiasi/Asosiasi.cpp
@@ -10,5 +10,9 @@ public:
 	pasien(string pNama) :nama(pNama) {
 		cout << "Pasien \"" << nama << "\" ada\n";
 	}
-
+	~pasien() {
+		cout << "Pasien \"" << nama << "\" tidak ada\n";
+	}
+	void tambahDokter(dokter*);
+	void
 };
