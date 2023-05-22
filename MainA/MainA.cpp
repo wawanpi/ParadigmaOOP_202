1f6e3e60e151acae8fbb3d184ea5a409eebf3054
wawanpi <muhaguskurniawan@gmail.com>
2023-05-22 16:06:57 +0700

    menyelesaikan program

--- MainA/MainA.cpp
+++ MainA/MainA.cpp
@@ -12,5 +12,20 @@ int main() {
 	anak* varAnak3 = new anak("Dewi");
 
 	varIbu->tambahAnak(varAnak1);
+	varIbu->tambahAnak(varAnak2);
+	varIbu2->tambahAnak(varAnak3);
+	varIbu2->tambahAnak(varAnak1);
+
+	varIbu->cetakAnak();
+	varIbu2->cetakAnak();
+
+	delete varIbu;
+	delete varIbu2;
+	delete varAnak1;
+	delete varAnak2;
+	delete varAnak3;
+
+	return 0;
+
 }
 
