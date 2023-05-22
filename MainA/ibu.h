bb31d7bf6ed5fe3361c34d54da819431e90bd924
wawanpi <muhaguskurniawan@gmail.com>
2023-05-22 15:48:50 +0700

    membuat acsesmodifay dan membuat perosuder

--- MainA/ibu.h
+++ MainA/ibu.h
@@ -1,10 +1,20 @@
 #pragma once
-#ifdef IBU_H
+#ifndef IBU_H
 #define IBU_H
 
 class ibu {
 public:
 	string nama;
+	vecktor<anak*> daftar_anak;
+
+	ibu(string pNama) :nama(pNama) {
+		cout << "Ibu \"" << nama << "\" ada\n";
+	}
+	~ibu() {
+		cout << "Ibu \"" << nama << "\" tidak ada\n";
+	}
+	void tambahAnak(anak*);
+	void cetakAnak();
 
 };
 #endif // IBU_H
