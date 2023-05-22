b4159660237876b20fdddf14eef5431385e5d349
wawanpi <muhaguskurniawan@gmail.com>
2023-05-22 15:26:28 +0700

    membuat  aceses modifay

--- ParadigmaOOP_202/ParadigmaOOP_202.cpp
+++ ParadigmaOOP_202/ParadigmaOOP_202.cpp
@@ -3,10 +3,19 @@
 using namespace std;
 
 class mahasiswa {
-	
 private:
 	const int id;
 	string nama;
 	float nilai;
+
+public:
+	mahasiswa(int pId, string pNama, float pNilai) :id(pId), nama(pNama), nilai(pNilai) {		//memberi im
+		//Defenisi
+	}
+
+	~mahasiswa() {
+		cout << "Id   " << id << endl;
+		cout
+	}
 };
 
