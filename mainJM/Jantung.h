5cf441d42c3afc1a5a4619f1fcddc22f036801cb
wawanpi <muhaguskurniawan@gmail.com>
2023-05-22 19:53:48 +0700

    membuat deklarasi jantung

--- mainJM/Jantung.h
+++ mainJM/Jantung.h
@@ -3,6 +3,14 @@
 
 using namespace std;
 
-class jantung
+class jantung {
+public:
+	jantung() {
+		cout << "jantung di hidupkan\n";
+	}
+	~jantung() {
+		cout << "jantung dimatikan\n";
+	}
+};
 #endif // !JANTUNG_H
 
