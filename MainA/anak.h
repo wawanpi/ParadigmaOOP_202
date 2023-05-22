03d8b2281382a4af74c4316f518c82afdce1305c
wawanpi <muhaguskurniawan@gmail.com>
2023-05-22 15:40:41 +0700

    membuat class di header anak

--- MainA/anak.h
+++ MainA/anak.h
@@ -2,5 +2,14 @@
 #ifndef ANAK_H
 #define ANAK_H
 
-class anak
-#endif // !ANAK_H
+class anak {
+public:
+	string nama;
+	anak(string pNama) :nama(pNama) {
+		cout << "Anak \"" << nama << "\" ada\n";
+	}
+	~anak() {
+		cout << "Anak \"" << nama << "\" tida ada\n";
+	}
+};
+#endif 
